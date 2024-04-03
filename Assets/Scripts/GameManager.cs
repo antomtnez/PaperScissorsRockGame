dae71632d4841b9eb0e3df6e807d9c7453d409c8
Anto Martínez <antomartinezdev@gmail.com>
2024-04-03 18:03:35 +0200

    I import TMPro to project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,4 +4,12 @@ public class GameManager : MonoBehaviour{
     public enum Choice { Rock, Paper, Scissors };
     public AIBehaviour aIBehaviour;
     public Choice playerChoice;
+
+    public void SetPlayerChoice(Choice choice){
+        playerChoice = choice;
+    }
+    
+    void Match(){
+
+    }
 }
