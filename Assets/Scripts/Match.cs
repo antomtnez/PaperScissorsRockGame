287b16a3d6ab099e451022943a59762e0e7923bc
Anto Martínez <antomartinezdev@gmail.com>
2024-04-04 17:16:24 +0200

    I update the match script to get better script sense

--- Assets/Scripts/Match.cs
+++ Assets/Scripts/Match.cs
@@ -1,68 +1,78 @@
 using UnityEngine;
 
-public class Match : MonoBehaviour
-{
-    public enum Choice { Rock, Paper, Scissors };
+public interface IHandConstestant{
+    public Choice GetChoice();
+    public void TakeDamage(int damageTaken);
+    public bool IsDead();
+}
 
-    private AIBehaviour m_AIBehaviour;
-    private Choice m_AIChoice;
-    private Choice m_PlayerChoice;
+public class Match : MonoBehaviour{
     [SerializeField] MatchCountdown m_MatchCountdown;
-    private Player m_Player;
-    private CPU m_CPU;
+    private IHandConstestant m_Player;
+    private IHandConstestant m_CPU;
+    private Choice m_PlayerChoice;
+    private Choice m_CPUChoice;
 
     void Start(){
-        m_AIBehaviour = new AIBehaviour();
-        m_Player = FindObjectOfType<Player>();
-        m_CPU = FindObjectOfType<CPU>();
+        m_Player = FindObjectOfType<Player>().GetComponent<IHandConstestant>();
+        m_CPU = FindObjectOfType<CPU>().GetComponent<IHandConstestant>();
     }
 
-    public void SetPlayerChoice(int choice){
-        m_PlayerChoice = (Choice)choice;
+    public void StartTurn(){
+        Debug.Log("Start new turn");
+        m_MatchCountdown.Init();
+        m_MatchCountdown.onCountdownFinished += EndTurn;
     }
 
-    void SetAIChoice(){
-        m_AIChoice = m_AIBehaviour.GetAIChoice();
+    void EndTurn(){
+        m_PlayerChoice = m_Player.GetChoice();
+        m_CPUChoice = m_CPU.GetChoice();
+
+        int result = TurnResult();
+
+        if(result == 0) m_CPU.TakeDamage(1);
+        if(result == 2) m_Player.TakeDamage(1);
+
+        Debug.Log($"You chose {m_PlayerChoice} and CPU chose {m_CPUChoice}. The result is {result}");
+        m_MatchCountdown.onCountdownFinished -= EndTurn;
+
+        GameManager.Instance.UpdateTurnHistory(new TurnHistoryEnter(){
+            playerChoice = m_PlayerChoice,
+            iAChoice = m_CPUChoice,
+            result = result
+        });
+
+        if(IsMatchFinished()){
+            if(m_Player.IsDead()){
+                Debug.Log("YOU LOSE!");
+            }else{
+                Debug.Log("YOU WIN!");
+            }
+        }else{
+            StartTurn();
+        }
     }
 
     //Check the moves chosen by player and ai and return the match result.
     //0 -> player win, 1 -> ties, 2 -> ai win
-    public void StartMatch(){
-        m_MatchCountdown.Init();
-        m_MatchCountdown.onCountdownFinished += ShowMeTheResult;
-        SetAIChoice();
-    }
-
-    int MatchResult(){
-        if(m_PlayerChoice == m_AIChoice) return 1;
+    int TurnResult(){
+        if(m_PlayerChoice == m_CPUChoice) return 1;
         if(m_PlayerChoice == Choice.Rock){
-            if(m_AIChoice == Choice.Paper)
+            if(m_CPUChoice == Choice.Paper)
                 return 2;
         }
         if(m_PlayerChoice == Choice.Paper){
-            if(m_AIChoice == Choice.Scissors)
+            if(m_CPUChoice == Choice.Scissors)
                 return 2;
         }
         if(m_PlayerChoice == Choice.Scissors){
-            if(m_AIChoice == Choice.Rock)
+            if(m_CPUChoice == Choice.Rock)
                 return 2;
         }
         return 0;
     }
 
-    void ShowMeTheResult(){
-        int result = MatchResult();
-
-        if(result == 0) m_CPU.TakeDamage(1);
-        if(result == 2) m_Player.TakeDamage(1);
-
-        m_AIBehaviour.AddMatchEntry(new RockPaperScissorsAI.MatchHistoryEnter(){
-            playerChoice = m_PlayerChoice,
-            iAChoice = m_AIChoice,
-            result = result
-        });
-
-        Debug.Log($"You chose {m_PlayerChoice} and CPU chose {m_AIChoice}. The result is {result}");
-        m_MatchCountdown.onCountdownFinished -= ShowMeTheResult;
+    bool IsMatchFinished(){
+        return m_Player.IsDead() || m_CPU.IsDead();
     }
 }
