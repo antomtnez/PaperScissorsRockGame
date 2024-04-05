9407fd87d18185780c5cfdc9c151947773b794ad
Anto Martínez <antomartinezdev@gmail.com>
2024-04-05 19:04:52 +0200

    I add a health bar decrease delayed effect to get better damage feeling

--- Assets/Scripts/UI/Base/HealthBarView.cs
+++ Assets/Scripts/UI/Base/HealthBarView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,6 +6,8 @@ using UnityEngine.UI;
 public class HealthBarView : MonoBehaviour{
     [SerializeField] TextMeshProUGUI NameText;
     [SerializeField] Slider HealthSlider;
+    [SerializeField] Slider DamagedSlider;
+    private float m_DamagedSliderTimer;
 
     protected void SetNameText(string name){
         NameText.SetText(name);
@@ -12,10 +15,32 @@ public class HealthBarView : MonoBehaviour{
 
     public void SetMaxHealth(int health){
         HealthSlider.maxValue  = health;
-        HealthSlider.value  = health;
+        HealthSlider.value  = HealthSlider.maxValue;
+        DamagedSlider.maxValue = health;
+        DamagedSlider.value = DamagedSlider.maxValue;
     }
 
     public void SetHealth(int health){
         HealthSlider.value  = health;
+        StartCoroutine(DamagedSliderDecreaseDelayed());
+    }
+    /*
+    void Update(){
+        m_DamagedSliderTimer -= Time.deltaTime;
+        if(m_DamagedSliderTimer <= 0){
+            if(HealthSlider.value < DamagedSlider.value){
+                float decreaseSpeed = 1f;
+                DamagedSlider.value -= decreaseSpeed * Time.deltaTime;
+            }
+        }
+    }
+    */
+
+    IEnumerator DamagedSliderDecreaseDelayed(){
+        yield return new WaitForSeconds(.5f);
+        while(DamagedSlider.value > HealthSlider.value){
+            yield return new WaitForSeconds(0.01f);
+            DamagedSlider.value -= 0.02f;
+        }
     }
 }
