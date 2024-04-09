7eb767beced013122f22505044c167e5abcaa6b3
Anto Martínez <antomartinezdev@gmail.com>
2024-04-09 20:47:16 +0200

    I add new sprites for countdown text and apply with some new animations on number changes

--- Assets/Scripts/UI/TurnCountdownView.cs
+++ Assets/Scripts/UI/TurnCountdownView.cs
@@ -1,18 +1,50 @@
-using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class TurnCountdownView : MonoBehaviour{
-    [SerializeField] TextMeshProUGUI CountdownText;
+    [SerializeField] GameObject CountdownPanel;
+    [SerializeField] Image NumberImage;
+    [SerializeField] List<Sprite> NumberImageList = new List<Sprite>();
+    private Vector3 m_StartPanelPosition;
+    private Vector3 m_FinishPanelPosition;
 
-    public void SetCountdownText(string text){
-        CountdownText.SetText(text);
+    void Start(){
+        m_StartPanelPosition = CountdownPanel.transform.localPosition;
+        m_FinishPanelPosition = CountdownPanel.transform.localPosition + new Vector3(200f, -300f, 0f);
     }
 
-    public void HideCountdownText(){
-        CountdownText.gameObject.SetActive(false);
+    public void SetCountdown(int count){
+        NumberImage.sprite = NumberImageList[count - 1];
+        PanelShake();
     }
 
-    public void ShowCountdownText(){
-        CountdownText.gameObject.SetActive(true);
+    public void HideCountdownPanel(){
+        HidePanel();
+    }
+
+    public void ShowCountdownPanel(){
+        ShowPanel();
+    }
+
+    public void PanelShake(){
+        CountdownPanel.transform.DOShakePosition(0.3f, new Vector2(20f, 20f));
+    }
+
+    public void ShowPanel(){
+        CountdownPanel.transform.DOKill();
+        CountdownPanel.SetActive(true);
+        CountdownPanel.transform.DOScale(Vector3.one, 0.2f);
+        CountdownPanel.transform.DOLocalMove(m_StartPanelPosition, 0.2f).OnComplete(()=>{
+            PanelShake();
+        });
+    }
+
+    public void HidePanel(){
+        CountdownPanel.transform.DOLocalMove(m_FinishPanelPosition, 0.2f);
+        CountdownPanel.transform.DOScale(Vector3.zero, 0.2f).OnComplete(()=>{
+            CountdownPanel.SetActive(false);
+        });
     }
 }
