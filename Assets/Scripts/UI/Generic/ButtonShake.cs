f3f889dc58953bc4104a58a961fc40f577270af1
Anto Martínez <antomartinezdev@gmail.com>
2024-04-14 19:38:35 +0200

    I import some sprites designs

--- Assets/Scripts/UI/Generic/ButtonShake.cs
+++ Assets/Scripts/UI/Generic/ButtonShake.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ButtonShake : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+    [SerializeField] float ShakeForce;
     public void OnPointerEnter(PointerEventData eventData){
         transform.DOScale(Vector3.one * 1.1f, 0.1f);
-        transform.DOShakePosition(0.1f, Vector3.one * 50f);
+        transform.DOShakePosition(0.1f, Vector3.one * ShakeForce);
     }
 
     public void OnPointerExit(PointerEventData eventData){
